6fc3981b3720634fab95749cbef38857c1c3bcf7
Hind <newhind2023@gmail.com>
2025-12-02 22:58:58 +0300

    Updated core app structure:
    - Added navbar.html
    - Added home.html (landing page UI)
    - Added base.html (layout)
    - Updated style.css (UI enhancements)
    - Added main.js (clear button)
    - Updated views.py (home_view)
    - Updated urls.py (home route)

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
 # Create your views here.
+
+def home_view(request):
+    return render(request, 'core/home.html')
